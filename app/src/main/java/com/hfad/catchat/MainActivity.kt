6f903861341c9e05307a875bffc4fbaa10b1f326
the-uncanny-valley <runcannyvalley@gmail.com>
2025-07-31 14:40:54 +0300

    feat: inflate toolbar menu in MainActivity with onCreateOptionsMenu

--- app/src/main/java/com/hfad/catchat/MainActivity.kt
+++ app/src/main/java/com/hfad/catchat/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.hfad.catchat
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.appbar.MaterialToolbar
@@ -12,5 +13,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
         setSupportActionBar(toolbar) // использовать панель инструментов как панель приложения
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_toolbar, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
